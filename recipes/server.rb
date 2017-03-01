30d7219b61dd91484b87082a0c0dadf40a68d729
nsrchemie <nsrchemie@gmail.com>
2017-03-01 21:48:10 +0000

    working with remote files and inseertion (in this case a jpg)

--- recipes/server.rb
+++ recipes/server.rb
@@ -9,6 +9,10 @@ package 'httpd' do
   action :install
 end
 
+remote_file '/var/www/html/profile.jpg' do
+ source 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/1/005/0a0/268/0507160.jpg'
+end
+
 template '/var/www/html/index.html' do
   source 'index.erb'
   action :create
