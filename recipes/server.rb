5c4ca3408cac25c22783e0cf6405f135259128aa
vagrant <vagrant@localhost.localdomain>
2017-03-01 01:13:17 +0000

    created resources for installing and running apache server with basic hello world html file

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,3 +3,16 @@
 # Recipe:: server
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
+
+
+package 'httpd' do
+  action :install
+end
+
+file '/var/www/html/index.html' do
+  content '<h2>Hello World!</h2>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
