f75e9c46a9f9359b4c1df8d3bc4cdbe470680ca8
nsrchemie <nsrchemie@gmail.com>
2017-03-05 19:13:59 +0000

    using subscribe to restart service (apache server) upon change in template state

--- recipes/server.rb
+++ recipes/server.rb
@@ -18,10 +18,10 @@ end
 template '/var/www/html/index.html' do
   source 'index.erb'
   action :create
-  notifies :restart, 'service[httpd]', :immediately
 end
 
 service 'httpd' do
   action [:enable, :start]
+  subscribes :restart, 'template[/var/www/html/index.html]', :immediately
 end
 
